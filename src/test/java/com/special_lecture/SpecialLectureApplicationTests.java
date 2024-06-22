b7f094aa4434f568f5ef0d1c07d408d36c2c8035
hyejin0662 <hyejin0776@gmail.com>
2024-06-22 19:46:48 +0900

    init

---
+++ src/test/java/com/special_lecture/SpecialLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.special_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
